39c2fbc0696e45e1a75f0ee0c648c7863ea40ff1
Huy Tran <huyserver1323@gmail.com>
2024-11-03 14:56:13 +0000

    fast_power in simp_main

--- Code_C++/simp_main.cpp
+++ Code_C++/simp_main.cpp
@@ -14,11 +14,14 @@ const int LimM = 1e6 + 5;
 const int INF = 1e18;
 const int MOD = 1e9 + 7;
 
-int a[LimN], b[LimN], sp[LimN], par[LimN];
+int power(int x, int n) {
+    if(n == 0) return 1;
+    if(n == 1) return x;
 
-int calc(int n){
-    if (!par[n]) return n;
-    return (par[n] = calc(par[n]));
+    int tmp = power(x, n / 2);
+    
+    if(n % 2 == 1) return tmp * tmp * x;
+    return tmp * tmp;
 }
 
 signed main() {
@@ -26,34 +29,8 @@ signed main() {
     // readInp("test.txt"); 
     // readInp("INPUT.INP");
     // writeOut("OUTPUT.OUT");
-    int x, n;
-    cin >> x >> n;
-    for(int i = 1; i <= n; i++){
-        cin >> a[i];
-        b[i] = a[i];
-    }
-
-    sort(b + 1, b + n + 1);
-    b[n + 1] = x;
-
-    int tmp = 0;
-    for(int i = 1; i <= n + 1; i++){
-        sp[i] = i - 1;
-        tmp = max(tmp, b[i] - b[i - 1]);
-    }
-
-    stack<int> st;
-    for(int i = n; i > 0; i--){
-        st.push(tmp);
-        int pos = lower_bound(b + 1, b + n + 1, a[i]) - b;
-        int res = calc(pos + 1);
-        tmp = max(tmp, b[res] - b[sp[pos]]);
-        sp[res] = sp[pos];
-        par[pos] = res;
-    }
-    while (!st.empty()){
-        cout << st.top() << "\n";
-        st.pop();
-    }
+    int a, b;
+    cin >> a >> b;
+    cout << power(a, b) << endl;
     return 0;
 }
