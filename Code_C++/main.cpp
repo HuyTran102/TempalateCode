722ace643b299c80387b47d91b14ebdcaa82a460
Huy Tran <huyserver1323@gmail.com>
2025-06-06 07:12:45 +0000

    Update code

--- Code_C++/main.cpp
+++ Code_C++/main.cpp
@@ -32,12 +32,40 @@ using namespace std;
 #define mapa(x, y) make_pair(x, y)
 #define TIME (1.0 * clock() / CLOCKS_PER_SEC)
 
-const int LimN = 45;
-const int LimM = 1e6 + 5;
+const int LimN = 1e2 + 5;
+const int LimM = 1e2 + 5;
+const int LimX = 1e6 + 5;
 const int INF = 1e18;
 const int MOD = 1e9 + 7;
-const int MAX = 1e6 + 5;
 const double dist = 0.0000001;
+
+template <class X, class Y>
+bool maximize(X &x, const Y &y) {
+    if(x < y) {
+        x = y;
+        return true;
+    }
+    return false;
+}
+
+template <class X, class Y>
+bool minimize(X &x, const Y &y) {
+    if(x > y) {
+        x = y;
+        return true;
+    }
+    return false;
+}
+
+void add(int &x, const int &y) {
+    x += y;
+    if(x >= MOD) x -= MOD;
+}
+
+void sub(int &x, const int &y) {
+    x -= y;
+    if(x >= MOD) x += MOD;
+}
 
 mt19937 rd(chrono::steady_clock::now().time_since_epoch().count());
 
@@ -46,19 +74,174 @@ int Rand(const int &l, const int &r) {
     int sz = (r - l + 1);
     return l + rd() % sz;
 }
- 
+
+// Chilling and coding
+
+class SegmentTree {
+
+private:
+
+    int n;
+    vi v, lazy;
+
+    void push(int id, int l, int r, int mid) {
+        if (lazy[id] == -1) return;
+
+        lazy[id * 2] = lazy[id];
+        lazy[id * 2 + 1] = lazy[id];
+
+        v[id * 2] = lazy[id] * (mid - l + 1);
+        v[id * 2 + 1] = lazy[id] * (r - mid);
+
+        lazy[id] = -1;
+
+    }
+
+    void update(int id, int l, int r, int sta, int fin, int val) {
+
+        if (sta > r || fin < l) {
+            return;
+        }
+
+        if (sta <= l && r <= fin) {
+
+            // v[id] = val * (r - l + 1);
+            v[id] = val;
+            lazy[id] = val;
+
+            return;
+
+        }
+
+        int mid = (l + r) / 2;
+
+        push(id, l, r, mid);
+
+        update(id * 2, l, mid, sta, fin, val);
+        update(id * 2 + 1, mid + 1, r, sta, fin, val);
+
+        v[id] = v[id * 2] + v[id * 2 + 1];
+
+    }
+
+
+
+    // int get(int id, int l, int r, int sta, int fin) {
+
+    //     if (sta > r || fin < l) {
+    //         return 0;
+    //     }
+
+    //     if (sta <= l && r <= fin) {
+    //         return v[id];
+    //     }
+
+    //     int mid = (l + r) / 2;
+
+    //     push(id, l, r, mid);
+
+    //     return __gcd(get(id * 2, l, mid, sta, fin), get(id * 2 + 1, mid + 1, r, sta, fin));
+
+    // }
+
+    int get(int id, int l, int r, int sta, int fin) {
+        if (sta > r || fin < l) {
+            return 0; // Out of range
+        }
+
+        if (sta <= l && r <= fin) {
+            return v[id]; // Fully in range
+        }
+
+        int mid = (l + r) / 2;
+
+        push(id, l, r, mid);
+
+        return get(id * 2, l, mid, sta, fin) + get(id * 2 + 1, mid + 1, r, sta, fin);
+    }
+
+public:
+
+    SegmentTree(int _n) {
+
+        n = _n;
+
+        v.resize(4 * n + 5, 0);
+
+        lazy.resize(4 * n + 5, 0);
+
+    }
+
+    void resize(int _n) {
+
+        n = _n;
+
+        v.resize(4 * n + 5, 0);
+
+        lazy.resize(4 * n + 5, 0);
+
+    }
+
+    void update(int sta, int fin, int val) {
+
+        update(1, 1, n, sta, fin, val);
+
+    }
+
+    int get(int sta, int fin) {
+        // return get(1, 1, n, sta, fin);
+        return get(1, 0, n - 1, sta, fin);
+    }
+
+};
+
+const int LENGTH = 1e6;
+const int BASE = 256;
+
+int pw[LENGTH + 5];
+
+struct Hash {
+    string s;
+    int n;
+    vector<int> pref;
+
+    void init(const int _n, const string &_s) {
+        s = _s;
+        n = _n;
+        s = "H" + s; // đưa về vị trí 1
+        pref.resize(n + 1, 0);
+    }
+
+    void calc() {
+        for (int i = 1; i <= n; i++) {
+            pref[i] = pref[i - 1] + s[i] * pw[i];
+            pref[i] %= MOD;
+        }
+    }
+
+    int get(int l, int r) {
+        return (pref[r] - pref[l - 1] + MOD) % MOD * pw[LENGTH - r] % MOD;
+    }
+};
+
+
 void solve() {
-    
+
+   
+
+
 }
 
 signed main() {
+    // readInp("soanhca.inp");
+    // writeOut("soanhca.out");
     // readInp("INPUT.INP");
     // writeOut("OUTPUT.OUT");
     // readInp("test.txt");
     fast_ios;
     bool PrintCase = 0;
     int _ = 1;
-    //cin >> _;
+    // cin >> _;
     for (int i = 1; i <= _; i++) {
         if (PrintCase) cout << "Case" << " " << i << ": ";
         solve();
@@ -71,4 +254,4 @@ signed main() {
 
 
 
-/* road to become a king*/
+/* road to become a king */
